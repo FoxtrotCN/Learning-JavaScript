034ea9e90337230d584dec7ade8c5bf1f954a971
Foxtrot <fernandoestarlin121000@gmail.com>
2022-06-24 16:18:13 -0400

    Working with Conditional Statements

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
+//
+// let num1 = 23;
+// let num2 = 13;
+//
+// document.write(num1 == num2);
+//
+// let num3 = 23;
+// let num4 = 14;
+//
+// document.write(num3 != num4);
+//
+// let num5 = 23;
+// let text = "23";
+//
+// document.write(num5 == text); // this return True
+//
+// // but this
+//
+// document.write(num5 === text); // return False ' = = = ' check if the data is identical.
+//
+// document.write(num1 > num2); // x > y ; x < y ; x >= y ; x <= y;
+//
+// // Logical Operators
+//
+// let value = true;
+// let value2 = true;
+//
+// let result = value && value2; // Both are true, return true.
+// let result2 = value || value2; // One true, all is true. Both false, return false.
+// let result3 = !value; // change the value: false -> true / true -> false.
+//
 
-let num1 = 23;
-let num2 = 13;
 
-document.write(num1 == num2);
+// Conditionals at JS
 
-let num3 = 23;
-let num4 = 14;
+if (10 > 15) { // if is true, this block is executed.
+    alert("Hello world!");
+}
 
-document.write(num3 != num4);
+else if (15 == 10) { // this block is executed just when the first condition is false.
+    alert("Hello world x2");
+}
 
-let num5 = 23;
-let text = "23";
-
-document.write(num5 == text); // this return True
-
-// but this
-
-document.write(num5 === text); // return False ' = = = ' check if the data is identical.
-
-document.write(num1 > num2); // x > y ; x < y ; x >= y ; x <= y;
-
-// Logical Operators
-
-let value = true;
-let value2 = true;
-
-let result = value && value2; // Both are true, return true.
-let result2 = value || value2; // One true, all is true. Both false, return false.
-let result3 = !value; // change the value: false -> true / true -> false.
-
+else {
+    alert("No more hello words..."); // When nothing is true.
+}
 
 
