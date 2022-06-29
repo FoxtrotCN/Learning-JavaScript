b93f33870071cabe2900c1a9de5dff569cde3247
Foxtrot <fernandoestarlin121000@gmail.com>
2022-06-29 10:27:49 -0400

    Cofla Story - Part 1. Making solutions

--- index.js
+++ index.js
@@ -1,48 +1,89 @@
-//
-// let num1 = 23;
-// let num2 = 13;
-//
-// document.write(num1 == num2);
-//
-// let num3 = 23;
-// let num4 = 14;
-//
-// document.write(num3 != num4);
-//
-// let num5 = 23;
-// let text = "23";
-//
-// document.write(num5 == text); // this return True
-//
-// // but this
-//
-// document.write(num5 === text); // return False ' = = = ' check if the data is identical.
-//
-// document.write(num1 > num2); // x > y ; x < y ; x >= y ; x <= y;
-//
-// // Logical Operators
-//
-// let value = true;
-// let value2 = true;
-//
-// let result = value && value2; // Both are true, return true.
-// let result2 = value || value2; // One true, all is true. Both false, return false.
-// let result3 = !value; // change the value: false -> true / true -> false.
-//
 
+moneyCofla = prompt("Cuanto dinero tienes Cofla?");
+moneyRoberto = prompt("Cuanto dinero tienes Roberto?");
+moneyPedro = prompt("Cuanto dinero tienes pedro?");
 
-// Conditionals at JS
-
-if (10 > 15) { // if is true, this block is executed.
-    alert("Hello world!");
+if (moneyCofla >= 0.6 && moneyCofla < 1) {
+    alert("Cofla, Compra el helado de agua");
 }
 
-else if (15 == 10) { // this block is executed just when the first condition is false.
-    alert("Hello world x2");
+else if (moneyCofla >= 1 && moneyCofla < 1.6) {
+    alert("Cofla, Compra el helado de crema");
+}
+
+else if (moneyCofla >= 1.6 && moneyCofla < 1.7) {
+    alert("Cofla, Compra el helado de heladix");
+}
+
+else if (moneyCofla >= 1.7 && moneyCofla < 1.8) {
+    alert("Cofla, Compra el helado de heladovich");
+}
+
+else if (moneyCofla >= 1.8 && moneyCofla < 2.9) {
+    alert(" Cofla, Compra el helado de helardo");
+}
+
+else if (moneyCofla >= 2.9) {
+    alert("Cofla, Compra el helado de Pote de 1/4 KG o con Confites");
 }
 
 else {
-    alert("No more hello words..."); // When nothing is true.
+    alert("Cofla, No te alcanza para ninguno. :(");
 }
 
+if (moneyRoberto >= 0.6 && moneyRoberto < 1) {
+    alert("Roberto, Compra el helado de agua");
+}
+
+else if (moneyRoberto >= 1 && moneyRoberto < 1.6) {
+    alert("Roberto, Compra el helado de crema");
+}
+
+else if (moneyRoberto >= 1.6 && moneyRoberto < 1.7) {
+    alert("Roberto, Compra el helado de heladix");
+}
+
+else if (moneyRoberto >= 1.7 && moneyRoberto < 1.8) {
+    alert("Roberto, Compra el helado de heladovich");
+}
+
+else if (moneyRoberto >= 1.8 && moneyRoberto < 2.9) {
+    alert("Roberto, Compra el helado de helardo");
+}
+
+else if (moneyRoberto >= 2.9) {
+    alert("Roberto, Compra el helado de Pote de 1/4 KG o con Confites");
+}
+
+else {
+    alert("Roberto, No te alcanza para ninguno," + " Roberto." +" :(");
+}
+
+if (moneyPedro >= 0.6 && moneyPedro < 1) {
+    alert("Pedro, Compra el helado de agua");
+}
+
+else if (moneyPedro>= 1 && moneyPedro < 1.6) {
+    alert("Pedro, Compra el helado de crema");
+}
+
+else if (moneyPedro >= 1.6 && moneyPedro < 1.7) {
+    alert("Compra el helado de heladix");
+}
+
+else if (moneyPedro >= 1.7 && moneyPedro < 1.8) {
+    alert("Pedro, Compra el helado de heladovich");
+}
+
+else if (moneyPedro >= 1.8 && moneyPedro < 2.9) {
+    alert("Pedro, Compra el helado de helardo");
+}
+
+else if (moneyPedro >= 2.9) {
+    alert("Pedro, Compra el helado de Pote de 1/4 KG o con Confites");
+}
+
+else {
+    alert("Pedro, No te alcanza para ninguno," + " Pedro." + " :(");
+}
 
