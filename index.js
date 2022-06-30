57ae2d495368ccffc06a79254eaaaee2ba1ce067
Foxtrot <fernandoestarlin121000@gmail.com>
2022-06-30 09:47:00 -0400

    Working with the bucle for

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 // Bucles and Iteration
 
- let number = 0;
-
-// while (number < 10) {
-//     number++;
-//     alert(number);
-// }
-
-// do {
-//
-//     alert(number);
-//     number++;
-// }
-//
-// while (number <= 6);
-
-while (number < 1000) {
-    number++;
-    document.write(number);
-    if (number == 10) {
-        break;
-    }
+for (let i = 0; i < 6; i++) {
+    document.write(i + "<br>");
 }
