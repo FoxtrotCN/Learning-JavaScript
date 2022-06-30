e8f45bc01185c00cda35c1a0f2ac181aaae97c7d
Foxtrot <fernandoestarlin121000@gmail.com>
2022-06-30 09:19:15 -0400

    Working with Associative arrays

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // ARRAYS
 
-let fruits = ["Banana", "Manzana", "Mango"];
+/*let fruits = ["Banana", "Manzana", "Mango"];*/
 
-alert(fruits[0]);
+/*alert(fruits[0]);
 alert(fruits[1]);
 alert(fruits[2]);
-alert(fruits[3]); // -> UNDEFINED, because we does not have an index 3.
+alert(fruits[3]);*/ // -> UNDEFINED, because we does not have an index 3.
 
+let pc = {
+    nombre: "Pc",
+    procesador: "Intel core i7",
+    ram: "16GB",
+    espacio: "1TB"
+};
+
+alert(pc["nombre"])
+phrase = `El nombre de mi pc es: ${pc.nombre}`;
+alert(phrase)
